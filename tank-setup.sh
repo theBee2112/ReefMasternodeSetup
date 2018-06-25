4dad9c91281c7504a3bf78bbdd2128b5f1542e52
theBee2112 <40008304+theBee2112@users.noreply.github.com>
2018-06-25 18:45:40 -0400

    last fix
    final fix

--- tank-setup.sh
+++ tank-setup.sh
@@ -171,13 +171,13 @@ rm -rf /usr/bin/tank*
 #Installing Daemon
  cd ~
 wget https://github.com/theBee2112/Tank/releases/download/untagged-eb82cb1a92c5221cb2a1/tank_beta_2_donation_fix.tar.gz
-tar -xzf tank_beta_2_donation_fix_linux.tar.gz -C ~/TankMasternodeSetup
-rm -rf tank_beta_2_donation_fix_linux.tar.gz
+tar -xzf tank_beta_2_donation_fix.tar.gz -C ~/TankMasternodeSetup
+rm -rf tank_beta_2_donation_fix.tar.gz
 
   stop_daemon
  
  # Deploy binaries to /usr/bin
- sudo cp ~/TankMasternodeSetup/tank_beta_2_donation_fix_linux* /usr/bin/
+ sudo cp ~/TankMasternodeSetup/tank_beta_2_donation_fix* /usr/bin/
  sudo chmod 755 -R ~/TankMasternodeSetup
  sudo chmod 755 /usr/bin/tank* 
  # Deploy masternode monitoring script
